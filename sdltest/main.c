d8854724c9ec7dd1774e92b3496d475976388871
majuba <mjbackhaus@gmail.com>
2018-04-03 12:55:41 +0200

    Initial Commit

---
+++ sdltest/main.c
@@ -0,0 +1,15 @@
+//
+//  main.c
+//  sdltest
+//
+//  Created by Mark Backhaus on 03.04.18.
+//  Copyright © 2018 Mark Backhaus. All rights reserved.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
